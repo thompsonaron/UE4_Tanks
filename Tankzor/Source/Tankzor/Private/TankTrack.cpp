482b3ed59a80ca06e6b48af3cfe2b80dc94b07fa
CHAOSINC <aronthompsonemail@gmail.com>
2019-03-13 04:29:48 +0100

    Basic movement/sliding

--- Tankzor/Source/Tankzor/Private/TankTrack.cpp
+++ Tankzor/Source/Tankzor/Private/TankTrack.cpp
@@ -13,6 +13,7 @@ void UTankTrack::SetThrottle(float Throttle) {
 	// TODO clamp actual throttle value so player can't over-drive
 	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
 	auto ForceLocation = GetComponentLocation();
-	//auto TankRoot = 
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
 
